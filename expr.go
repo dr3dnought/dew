b7ad3395bc927a2eb214b6faca9c551d6e4d868f
dr3dnought <sheremetyev48@mail.ru>
2025-11-28 02:49:04 +0300

    feat(expr): Add agragate functions and alias expression

--- expr.go
+++ expr.go
@@ -156,11 +156,9 @@ func (s *sortExpr) Sql() string {
 }
 
 func (s *sortExpr) Args() []any {
-	// В простой сортировке аргументов нет
 	return nil
 }
 
-// Asc возвращает Expression для сортировки по возрастанию
 func Asc(col any) Expression {
 	return &sortExpr{
 		column: fmt.Sprintf("%v", col),
@@ -168,13 +166,55 @@ func Asc(col any) Expression {
 	}
 }
 
-// Desc возвращает Expression для сортировки по убыванию
 func Desc(col any) Expression {
 	return &sortExpr{
 		column: fmt.Sprintf("%v", col),
 		dir:    "DESC",
 	}
 }
+
+// Agregate Funtions //
+
+type AGREGATE_FUNCTION_TYPE string
+
+const (
+	SUM AGREGATE_FUNCTION_TYPE = "SUM"
+	AVG AGREGATE_FUNCTION_TYPE = "AVG"
+	MAX AGREGATE_FUNCTION_TYPE = "MAX"
+	MIN AGREGATE_FUNCTION_TYPE = "MIN"
+)
+
+type aggExpr struct {
+	fnType AGREGATE_FUNCTION_TYPE
+	col    string
+}
+
+func (a *aggExpr) Sql() string { return fmt.Sprintf("%s(%s)", a.fnType, a.col) }
+
+func (a *aggExpr) Args() []any { return nil }
+
+func Sum(col Expression) Expression { return &aggExpr{fnType: SUM, col: col.Sql()} }
+
+func Avg(col Expression) Expression { return &aggExpr{fnType: AVG, col: col.Sql()} }
+
+func Max(col Expression) Expression { return &aggExpr{fnType: MAX, col: col.Sql()} }
+
+func Min(col Expression) Expression { return &aggExpr{fnType: MIN, col: col.Sql()} }
+
+// * Alias Expr * //
+
+type aliasExpr struct {
+	expr  Expression
+	alias string
+}
+
+func (a *aliasExpr) Sql() string { return fmt.Sprintf("%s AS %s", a.expr.Sql(), a.alias) }
+
+func (a *aliasExpr) Args() []any { return a.expr.Args() }
+
+func As(exp Expression, alias string) Expression { return &aliasExpr{expr: exp, alias: alias} }
+
+// * Raw Expr * //
 
 type rawExpr struct {
 	sql  string
