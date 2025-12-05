cc8d6652114ec52180f7c28a1bd5a5ba9ce961cb
dr3dnought <sheremetyev48@mail.ru>
2025-12-05 16:13:39 +0300

    feat(db): add Dialect

--- db.go
+++ db.go
@@ -4,6 +4,7 @@ import "database/sql"
 
 type DB struct {
 	*sql.DB
+	dialect Dialect
 }
 
 func Open(driverName, dataSourceName string) (*DB, error) {
@@ -11,5 +12,26 @@ func Open(driverName, dataSourceName string) (*DB, error) {
 	if err != nil {
 		return nil, err
 	}
-	return &DB{db}, nil
+
+	var dialect Dialect
+	switch driverName {
+	case "postgres", "pgx":
+		dialect = PostgreSQLDialect{}
+	case "sqlserver", "mssql":
+		dialect = MSSQLDialect{}
+	case "mysql":
+		dialect = MySQLDialect{}
+	default:
+		dialect = SQLiteDialect{}
+	}
+
+	return &DB{db, dialect}, nil
+}
+
+func OpenWithDialect(driverName, dataSourceName string, dialect Dialect) (*DB, error) {
+	db, err := sql.Open(driverName, dataSourceName)
+	if err != nil {
+		return nil, err
+	}
+	return &DB{db, dialect}, nil
 }
